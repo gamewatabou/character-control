86f5c889034a47c9a03c1d3ebf8197722148fcee
gamewatabou <game.watabou@gmail.com>
2021-03-17 00:28:09 +0900

    カメラがキャラクターを追従するように

---
+++ Assets/Scripts/CameraControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerScript : MonoBehaviour
+{
+    // プレイヤーオブジェクト
+    public GameObject player;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        // プレイヤーの位置を取得
+        Vector3 playerPos = this.player.transform.position;
+        float znorm = (playerPos.z + 2);
+        float ynorm = (playerPos.y + 2);
+
+        // プレイヤーの位置をカメラに反映
+        transform.position = new Vector3(playerPos.x, ynorm, znorm);
+    }
+}
