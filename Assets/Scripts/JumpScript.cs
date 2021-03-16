4a3ccda91538d4da2a06ef51c97b40738d90697d
gamewatabou <game.watabou@gmail.com>
2021-03-17 01:46:53 +0900

    ボタン押下でキャラクターがジャンプするように

---
+++ Assets/Scripts/JumpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class JumpScript : MonoBehaviour
+{
+    public GameObject player;
+    private ThirdPersonCharacter chara;
+
+    void Start()
+    {
+        chara = player.GetComponent<ThirdPersonCharacter>();
+    }
+
+    public void OnClick()
+    {
+        Vector3 direction = new Vector3(0, 0, 0);
+        chara.Move(direction, false, true);
+    }
+}
