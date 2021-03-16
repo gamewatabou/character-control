65a59d52a891b65610076d857597285d06b8620a
gamewatabou <game.watabou@gmail.com>
2021-03-17 01:35:48 +0900

    ジョイスティックでキャラクターを動かせるように

--- Assets/Scripts/CameraControllerScript.cs
+++ Assets/Scripts/CameraControllerScript.cs
@@ -15,8 +15,8 @@ public class CameraControllerScript : MonoBehaviour
     {
         // プレイヤーの位置を取得
         Vector3 playerPos = this.player.transform.position;
-        float znorm = (playerPos.z + 2);
-        float ynorm = (playerPos.y + 2);
+        float znorm = (playerPos.z - 2);
+        float ynorm = (playerPos.y + 2f);
 
         // プレイヤーの位置をカメラに反映
         transform.position = new Vector3(playerPos.x, ynorm, znorm);
---
+++ Assets/Scripts/MobileControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class MobileControllerScript : MonoBehaviour
+{
+    public FixedJoystick joystick;
+    public GameObject player;
+    private ThirdPersonCharacter chara;
+
+    void Start()
+    {
+        chara = player.GetComponent<ThirdPersonCharacter>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        chara.Move(direction, false, false);
+    }
+}
